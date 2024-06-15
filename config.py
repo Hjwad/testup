c09f029e74fe3010f64863005dda29fa9c213a73
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-06-15 19:11:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ from os import getenv
 
 from dotenv import load_dotenv
 from pyrogram import filters
+import bmemcached
+import os
 
 load_dotenv()
 
@@ -53,7 +55,14 @@ AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT","false"))
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+#mem
+servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
+user = os.environ.get('MEMCACHIER_USERNAME', '')
+passw = os.environ.get('MEMCACHIER_PASSWORD', '')
 
+mc = bmemcached.Client(servers, username=user, password=passw)
+
+mc.enable_retry_delay(True)
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
